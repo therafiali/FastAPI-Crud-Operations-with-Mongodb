c71c7519d6a4ed98c90ee4e4b47d4f2f1f105c7d
Muhammad Rafi Ali <therafiali@gmail.com>
2024-08-14 21:45:36 +0500

    add create user route

--- database/models.py
+++ database/models.py
@@ -1,14 +1,14 @@
-# create model for pass data in database
+# # create model for pass data in database
 from pydantic import BaseModel
 from datetime import datetime
 
-class Todo(BaseModel):
-    title : str
-    description : str
-    is_completed : bool = False
-    is_deleted : bool = False
-    updated_at : int = int(datetime.timestamp(datetime.now()))
-    creation : int = int(datetime.timestamp(datetime.now()))
+# class Todo(BaseModel):
+#     title : str
+#     description : str
+#     is_completed : bool = False
+#     is_deleted : bool = False
+#     updated_at : int = int(datetime.timestamp(datetime.now()))
+#     creation : int = int(datetime.timestamp(datetime.now()))
     
 class User(BaseModel):
     name : str
@@ -16,3 +16,22 @@ class User(BaseModel):
     password : str
     updated_at : int = int(datetime.timestamp(datetime.now()))
     creation : int = int(datetime.timestamp(datetime.now()))
+    
+    
+# from pydantic import BaseModel
+
+# class Token(BaseModel):
+#     access_token: str
+#     token_type: str
+
+# class TokenData(BaseModel):
+#     username: str | None = None
+
+# class User(BaseModel):
+#     username: str
+#     email: str | None = None
+#     full_name: str | None = None
+#     disabled: bool | None = None
+
+# class UserInDB(User):
+#     hashed_password: str    
--- main.py
+++ main.py
@@ -1,75 +1,373 @@
-from fastapi import FastAPI, APIRouter, HTTPException
-from database.schemas import all_tasks, all_users
-from database.models import Todo, User
-from configrations import collection, usertable
-from bson.objectid import ObjectId
-from datetime import datetime
+# from fastapi import FastAPI, APIRouter, HTTPException
+# from database.schemas import all_tasks, all_users
+# from database.models import Todo, User
+# from configrations import collection, usertable
+# from bson.objectid import ObjectId
+# from datetime import datetime
+# from datetime import datetime, timedelta, timezone
+# from typing import Annotated
+# from fastapi import Depends, FastAPI, HTTPException, status
+# from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+# from pydantic import BaseModel
 
+
+# SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+# ALGORITHM = "HS256"
+# ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+# fake_users_db = {
+#     "johndoe": {
+#         "username": "johndoe",
+#         "full_name": "John Doe",
+#         "email": "johndoe@example.com",
+#         "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+#         "disabled": False,
+#     }
+# }
+
+
+# class Token(BaseModel):
+#     access_token: str
+#     token_type: str
+
+
+# class TokenData(BaseModel):
+#     username: str | None = None
+
+
+# class User(BaseModel):
+#     username: str
+#     email: str | None = None
+#     full_name: str | None = None
+#     disabled: bool | None = None
+
+
+# class UserInDB(User):
+#     hashed_password: str
+
+
+# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+# app = FastAPI()
+# router = APIRouter()
+
+
+# @router.get("/")
+# async def get_all_todos():
+#     data = collection.find()
+#     return all_tasks(data)
+
+
+# @router.post("/")
+# async def create_task(new_task: Todo):
+#     try:
+#         response = collection.insert_one(dict(new_task))
+#         return {"Status code": 200, "id": str(response.inserted_id)}
+#     except Exception as e:
+
+#         return HTTPException(status_code=500, detail=f"The Error is: {e}")
+
+
+# @router.put("/{task_id}")
+# async def update_task(task_id: str, updated_task: Todo):
+#     try:
+#         id = ObjectId(task_id)
+#         existing_data = collection.find_one({"_id": id, "is_deleted": False})
+#         if not existing_data:
+#             return HTTPException(status_code=404, detail=f"Task does not exist")
+#         updated_task.updated_at = datetime.timestamp(datetime.now())
+#         response = collection.update_one(
+#             {"_id": id}, {"$set": dict(updated_task)})
+#         return {"Status code": 200, "message": "task update succesfully"}
+#     except Exception as e:
+
+#         return HTTPException(status_code=500, detail=f"The Error is: {e}")
+
+
+# @router.delete("/{task_id}")
+# async def delete_task(task_id: str):
+#     try:
+#         id = ObjectId(task_id)
+#         existing_data = collection.find_one({"_id": id, "is_deleted": False})
+#         if not existing_data:
+#             return HTTPException(status_code=404, detail=f"Task does not exist")
+#         response = collection.delete_one({"_id": id})
+#         return {"Status code": 200, "message": "task delete succesfully"}
+#     except Exception as e:
+
+#         return HTTPException(status_code=500, detail=f"The Error is: {e}")
+
+
+# @router.get("/users")
+# async def get_all_users():
+#     data = usertable.find()
+#     return all_users(data)
+
+
+# @router.post("/create_user")
+# async def create_user(new_task: User):
+#     try:
+#         response = usertable.insert_one(dict(new_task))
+#         return {"Status code": 200, "id": str(response.inserted_id)}
+#     except Exception as e:
+
+#         return HTTPException(status_code=500, detail=f"The Error is: {e}")
+
+# def verify_password(plain_password, hashed_password):
+#     return pwd_context.verify(plain_password, hashed_password)
+
+
+# def get_password_hash(password):
+#     return pwd_context.hash(password)
+
+
+# def get_user(db, username: str):
+#     if username in db:
+#         user_dict = db[username]
+#         return UserInDB(**user_dict)
+
+
+# def authenticate_user(fake_db, username: str, password: str):
+#     user = get_user(fake_db, username)
+#     if not user:
+#         return False
+#     if not verify_password(password, user.hashed_password):
+#         return False
+#     return user
+
+
+# def create_access_token(data: dict, expires_delta: timedelta | None = None):
+#     to_encode = data.copy()
+#     if expires_delta:
+#         expire = datetime.now(timezone.utc) + expires_delta
+#     else:
+#         expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+#     to_encode.update({"exp": expire})
+#     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+#     return encoded_jwt
+
+
+# async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+#     credentials_exception = HTTPException(
+#         status_code=status.HTTP_401_UNAUTHORIZED,
+#         detail="Could not validate credentials",
+#         headers={"WWW-Authenticate": "Bearer"},
+#     )
+#     try:
+#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+#         username: str = payload.get("sub")
+#         if username is None:
+#             raise credentials_exception
+#         token_data = TokenData(username=username)
+#     except InvalidTokenError:
+#         raise credentials_exception
+#     user = get_user(fake_users_db, username=token_data.username)
+#     if user is None:
+#         raise credentials_exception
+#     return user
+
+
+# async def get_current_active_user(
+#     current_user: Annotated[User, Depends(get_current_user)],
+# ):
+#     if current_user.disabled:
+#         raise HTTPException(status_code=400, detail="Inactive user")
+#     return current_user
+
+
+# @app.post("/token")
+# async def login_for_access_token(
+#     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+# ) -> Token:
+#     user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+#     if not user:
+#         raise HTTPException(
+#             status_code=status.HTTP_401_UNAUTHORIZED,
+#             detail="Incorrect username or password",
+#             headers={"WWW-Authenticate": "Bearer"},
+#         )
+#     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+#     access_token = create_access_token(
+#         data={"sub": user.username}, expires_delta=access_token_expires
+#     )
+#     return Token(access_token=access_token, token_type="bearer")
+
+
+# @app.get("/users/me/", response_model=User)
+# async def read_users_me(
+#     current_user: Annotated[User, Depends(get_current_active_user)],
+# ):
+#     return current_user
+
+
+# @app.get("/users/me/items/")
+# async def read_own_items(
+#     current_user: Annotated[User, Depends(get_current_active_user)],
+# ):
+#     return [{"item_id": "Foo", "owner": current_user.username}]
+
+
+# app.include_router(router)
+
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+
+import jwt
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jwt.exceptions import InvalidTokenError
+from passlib.context import CryptContext
+from pydantic import BaseModel
+from configrations import usertable
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    }
+}
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
-router = APIRouter()
 
 
-@router.get("/")
-async def get_all_todos():
-    data = collection.find()
-    return all_tasks(data)
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
 
 
-@router.post("/")
-async def create_task(new_task: Todo):
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
     try:
-        response = collection.insert_one(dict(new_task))
-        return {"Status code": 200, "id": str(response.inserted_id)}
-    except Exception as e:
-
-        return HTTPException(status_code=500, detail=f"The Error is: {e}")
-
-
-@router.put("/{task_id}")
-async def update_task(task_id: str, updated_task: Todo):
-    try:
-        id = ObjectId(task_id)
-        existing_data = collection.find_one({"_id": id, "is_deleted": False})
-        if not existing_data:
-            return HTTPException(status_code=404, detail=f"Task does not exist")
-        updated_task.updated_at = datetime.timestamp(datetime.now())
-        response = collection.update_one(
-            {"_id": id}, {"$set": dict(updated_task)})
-        return {"Status code": 200, "message": "task update succesfully"}
-    except Exception as e:
-
-        return HTTPException(status_code=500, detail=f"The Error is: {e}")
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
 
 
-@router.delete("/{task_id}")
-async def delete_task(task_id: str):
-    try:
-        id = ObjectId(task_id)
-        existing_data = collection.find_one({"_id": id, "is_deleted": False})
-        if not existing_data:
-            return HTTPException(status_code=404, detail=f"Task does not exist")
-        response = collection.delete_one({"_id": id})
-        return {"Status code": 200, "message": "task delete succesfully"}
-    except Exception as e:
-
-        return HTTPException(status_code=500, detail=f"The Error is: {e}")
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
 
 
-@router.get("/users")
-async def get_all_users():
-    data = usertable.find()
-    return all_users(data)
+@app.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
 
 
-@router.post("/create_user")
-async def create_user(new_task: User):
-    try:
-        response = usertable.insert_one(dict(new_task))
-        return {"Status code": 200, "id": str(response.inserted_id)}
-    except Exception as e:
-
-        return HTTPException(status_code=500, detail=f"The Error is: {e}")
+@app.get("/users/me/", response_model=User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return current_user
 
 
-app.include_router(router)
+@app.get("/users/me/items/")
+async def read_own_items(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return [{"item_id": "Foo", "owner": current_user.username}]
+
+@app.post("/create_user/")
+async def create_user(user: User, current_user: User = Depends(get_current_user)):
+    if current_user.username == "ali":
+        # Create the user in the database
+        response = usertable.insert_one(dict(user))
+        
+        return {"status": "success", "user_id": "yes"}
+    else:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Unauthorized")
